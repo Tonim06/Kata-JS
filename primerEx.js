324adf79207278f55da90e7265182a5695e0164f
Pep Guiem <pepguiemnolla@gmail.com>
2023-03-06 20:00:59 +0100

    PirmerEx

---
+++ primerEx.js
@@ -0,0 +1,8 @@
+const nom = document.querySelector('#Nom');
+const boto = document.querySelector('button');
+const resultat = document.querySelector('#resultat');
+
+
+boto.onclick = function(){
+    resultat.value = ((nom.value.replaceAll(/ /g,'')).toLowerCase()).replaceAll('à','a').replaceAll('á','a').replaceAll('è','e').replaceAll('é','e').replaceAll('ù','u').replaceAll('ú','u').replaceAll('ò','o').replaceAll('ó','o').replaceAll('ì','i').replaceAll('í','i')
+}
