0c807acb2fb10d37e18288ed415798bbfb71b9fa
Pep Guiem <pepguiemnolla@gmail.com>
2023-03-06 20:01:49 +0100

    si

--- primerEx.js
+++ primerEx.js
@@ -4,5 +4,5 @@ const resultat = document.querySelector('#resultat');
 
 
 boto.onclick = function(){
-    resultat.value = ((nom.value.replaceAll(/ /g,'')).toLowerCase()).replaceAll('à','a').replaceAll('á','a').replaceAll('è','e').replaceAll('é','e').replaceAll('ù','u').replaceAll('ú','u').replaceAll('ò','o').replaceAll('ó','o').replaceAll('ì','i').replaceAll('í','i')
+    resultat.value = ((nom.value.replaceAll(/ /g,'')).toLowerCase()).replaceAll('à','a').replaceAll('á','a').replaceAll('è','e').replaceAll('é','e').replaceAll('ù','u').replaceAll('ú','u').replaceAll('ò','o').replaceAll('ó','o').replaceAll('ì','i').replaceAll('í','i');
 }
